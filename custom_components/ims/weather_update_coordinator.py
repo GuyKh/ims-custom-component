49d81eda7e1c293502968bd1f97e93bada7cb34e
Guy Khmelnitsky <guykhmel@gmail.com>
2024-01-16 12:42:59 +0200

    Clean config_flow.py and weather_update_coordinator.py

--- custom_components/ims/weather_update_coordinator.py
+++ custom_components/ims/weather_update_coordinator.py
@@ -1,19 +1,11 @@
 """Weather data coordinator for the OpenWeatherMap (OWM) service."""
-from datetime import timedelta, datetime, date
-import logging
-
 import asyncio
+import logging
+from datetime import timedelta, date
+
 import async_timeout
-
-import json
-
-import voluptuous as vol
-
-from homeassistant.helpers import sun
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
-from homeassistant.util import dt
 from weatheril import *
-
 
 from .const import (
     DOMAIN,
@@ -61,32 +53,33 @@ class WeatherUpdateCoordinator(DataUpdateCoordinator):
         current_weather = await loop.run_in_executor(
             None, self.weather.get_current_analysis
         )
-        forecast = await loop.run_in_executor(None, self.weather.get_forecast)
+        weather_forecast = await loop.run_in_executor(None, self.weather.get_forecast)
         images = await loop.run_in_executor(None, self.weather.get_radar_images)
 
         _LOGGER.debug("Data fetched from IMS of %s", current_weather.forecast_time.strftime("%m/%d/%Y, %H:%M:%S"))
 
-        self._filter_future_forecast(forecast)
-        return WeatherData(current_weather, forecast, images)
+        self._filter_future_forecast(weather_forecast)
+        return WeatherData(current_weather, weather_forecast, images)
 
-    def _filter_future_forecast(self, forecast):
+    @staticmethod
+    def _filter_future_forecast(weather_forecast):
         """ Filter Forecast to include only future dates """
         today_datetime = datetime.fromordinal(date.today().toordinal())
-        filtered_day_list = list(filter(lambda daily_forecast: daily_forecast.date >= today_datetime, forecast.days))
+        filtered_day_list = list(filter(lambda daily: daily.date >= today_datetime, weather_forecast.days))
 
         for daily_forecast in filtered_day_list:
             filtered_hours = []
             for hourly_forecast in daily_forecast.hours:
-                forecast_datetime = daily_forecast.date + timedelta(hours = int(hourly_forecast.hour.split(":")[0]))
-                if (datetime.now() <= forecast_datetime):
+                forecast_datetime = daily_forecast.date + timedelta(hours=int(hourly_forecast.hour.split(":")[0]))
+                if datetime.now() <= forecast_datetime:
                     filtered_hours.append(hourly_forecast)
             daily_forecast.hours = filtered_hours
 
-        forecast.days = filtered_day_list
+        weather_forecast.days = filtered_day_list
 
 
 class WeatherData:
-    def __init__(self, current_weather, forecast, images):
+    def __init__(self, current_weather, weather_forecast, images):
         self.current_weather = current_weather
-        self.forecast = forecast
+        self.forecast = weather_forecast
         self.images = images
