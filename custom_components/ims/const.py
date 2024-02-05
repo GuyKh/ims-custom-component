a5584c8b6b7e7e80ddb9dcb9d595406218a18b44
Guy Khmelnitsky <3136012+GuyKh@users.noreply.github.com>
2024-02-05 09:56:07 +0200

    0.1.24 - Updated icon mapping

--- custom_components/ims/const.py
+++ custom_components/ims/const.py
@@ -8,6 +8,7 @@ from homeassistant.components.weather import (
     ATTR_CONDITION_HAIL,
     ATTR_CONDITION_LIGHTNING_RAINY,
     ATTR_CONDITION_PARTLYCLOUDY,
+    ATTR_CONDITION_POURING,
     ATTR_CONDITION_RAINY,
     ATTR_CONDITION_SNOWY,
     ATTR_CONDITION_SNOWY_RAINY,
@@ -34,25 +35,25 @@ CONF_IMAGES_PATH = "images_path"
 CONFIG_FLOW_VERSION = 2
 ENTRY_NAME = "name"
 ENTRY_WEATHER_COORDINATOR = "weather_coordinator"
-ATTR_API_PRECIPITATION = "precipitation"
-ATTR_API_FORECAST_TIME = "forecast_time"
 ATTR_API_DEW_POINT = "due_point_Temp"
-ATTR_API_TEMPERATURE = "temperature"
 ATTR_API_FEELS_LIKE_TEMPERATURE = "feels_like"
-ATTR_API_WIND_SPEED = "wind_speed"
-ATTR_API_WIND_BEARING = "wind_direction_id"
-ATTR_API_WIND_CHILL = "wind_chill"
-ATTR_API_RELATIVE_HUMIDITY = "relative_humidity"
+ATTR_API_FORECAST_DATE = "forecast_date"
+ATTR_API_FORECAST_TIME = "forecast_time"
+ATTR_API_HEAT_STRESS = "heat_stress"
+ATTR_API_HEAT_STRESS_LEVEL = "heat_stress_level"
+ATTR_API_MAXIMUM_TEMPERATURE = "maximum_temperature"
+ATTR_API_MAXIMUM_UV_INDEX = "maximum_uvi"
+ATTR_API_MINIMUM_TEMPERATURE = "minimum_temperature"
+ATTR_API_PRECIPITATION = "precipitation"
 ATTR_API_RAIN = "rain"
+ATTR_API_RELATIVE_HUMIDITY = "relative_humidity"
+ATTR_API_TEMPERATURE = "temperature"
 ATTR_API_UV_INDEX = "u_v_index"
 ATTR_API_UV_LEVEL = "u_v_level"
 ATTR_API_WEATHER_CODE = "weather_code"
-ATTR_API_HEAT_STRESS = "heat_stress"
-ATTR_API_HEAT_STRESS_LEVEL = "heat_stress_level"
-ATTR_API_FORECAST_DATE = "forecast_date"
-ATTR_API_MAXIMUM_TEMPERATURE = "maximum_temperature"
-ATTR_API_MINIMUM_TEMPERATURE = "minimum_temperature"
-ATTR_API_MAXIMUM_UV_INDEX = "maximum_uvi"
+ATTR_API_WIND_BEARING = "wind_direction_id"
+ATTR_API_WIND_CHILL = "wind_chill"
+ATTR_API_WIND_SPEED = "wind_speed"
 UPDATE_LISTENER = "update_listener"
 PLATFORMS = [Platform.SENSOR, Platform.WEATHER]
 IMS_PLATFORMS = ["Sensor", "Weather"]
@@ -64,27 +65,11 @@ FORECAST_MODE_DAILY = "daily"
 DEFAULT_FORECAST_MODE = FORECAST_MODE_DAILY
 FORECAST_MODES = [FORECAST_MODE_HOURLY, FORECAST_MODE_DAILY]
 
+TYPE_CITY = "city"
 TYPE_CURRENT_UV_INDEX = "current_uv_index"
 TYPE_CURRENT_UV_LEVEL = "current_uv_level"
-TYPE_MAX_UV_INDEX = "max_uv_index"
-TYPE_HEAT_STRESS = "heat_stress"
-TYPE_HEAT_STRESS_LEVEL = "heat_stress_level"
-TYPE_HUMIDITY = "humidity"
 TYPE_DEW_POINT_TEMP = "dew_point_temp"
-TYPE_IS_RAINING = "is_raining"
-TYPE_TEMPERATURE = "temperature"
 TYPE_FEELS_LIKE = "feels_like"
-TYPE_MIN_TEMP = "min_temp"
-TYPE_MAX_TEMP = "max_temp"
-TYPE_WAVE_HEIGHT = "wave_height"
-TYPE_WIND_DIRECTION = "wind_direction"
-TYPE_WIND_SPEED = "wind_speed"
-TYPE_WIND_CHILL = "wind_chill"
-TYPE_WEATHER_CODE = "weather_code"
-TYPE_CITY = "city"
-TYPE_FORECAST_TIME = "forecast_time"
-TYPE_PRECIPITATION = "precipitation"
-TYPE_PRECIPITATION_PROBABILITY = "precipitation_probability"
 TYPE_FORECAST_PREFIX = "forecast_"
 TYPE_FORECAST_TODAY = "today"
 TYPE_FORECAST_DAY1 = "day1"
@@ -94,28 +79,44 @@ TYPE_FORECAST_DAY4 = "day4"
 TYPE_FORECAST_DAY5 = "day5"
 TYPE_FORECAST_DAY6 = "day6"
 TYPE_FORECAST_DAY7 = "day7"
+TYPE_FORECAST_TIME = "forecast_time"
+TYPE_HEAT_STRESS = "heat_stress"
+TYPE_HEAT_STRESS_LEVEL = "heat_stress_level"
+TYPE_HUMIDITY = "humidity"
+TYPE_IS_RAINING = "is_raining"
+TYPE_MAX_TEMP = "max_temp"
+TYPE_MAX_UV_INDEX = "max_uv_index"
+TYPE_MIN_TEMP = "min_temp"
+TYPE_PRECIPITATION = "precipitation"
+TYPE_PRECIPITATION_PROBABILITY = "precipitation_probability"
+TYPE_TEMPERATURE = "temperature"
+TYPE_WAVE_HEIGHT = "wave_height"
+TYPE_WEATHER_CODE = "weather_code"
+TYPE_WIND_CHILL = "wind_chill"
+TYPE_WIND_DIRECTION = "wind_direction"
+TYPE_WIND_SPEED = "wind_speed"
 
+FIELD_NAME_DEW_POINT_TEMP = "due_point_Temp"
+FIELD_NAME_FEELS_LIKE = "feels_like"
 FIELD_NAME_FORECAST_TIME = "forecast_time"
 FIELD_NAME_HEAT_STRESS = "heat_stress"
 FIELD_NAME_HEAT_STRESS_LEVEL = "heat_stress_level"
 FIELD_NAME_HUMIDITY = "relative_humidity"
-FIELD_NAME_DEW_POINT_TEMP = "due_point_Temp"
+FIELD_NAME_LOCATION = "location"
+FIELD_NAME_MAX_TEMP = "max_temp"
+FIELD_NAME_MIN_TEMP = "min_temp"
 FIELD_NAME_RAIN = "rain"
 FIELD_NAME_RAIN_CHANCE = "rain_chance"
 FIELD_NAME_TEMPERATURE = "temperature"
-FIELD_NAME_FEELS_LIKE = "feels_like"
-FIELD_NAME_MIN_TEMP = "min_temp"
-FIELD_NAME_MAX_TEMP = "max_temp"
+FIELD_NAME_UV_INDEX = "u_v_index"
+FIELD_NAME_UV_INDEX_FACTOR = "u_v_i_factor"
+FIELD_NAME_UV_INDEX_MAX = "u_v_i_max"
+FIELD_NAME_UV_LEVEL = "u_v_level"
 FIELD_NAME_WAVE_HEIGHT = "wave_height"
+FIELD_NAME_WEATHER_CODE = "weather_code"
+FIELD_NAME_WIND_CHILL = "wind_chill"
 FIELD_NAME_WIND_DIRECTION_ID = "wind_direction_id"
 FIELD_NAME_WIND_SPEED = "wind_speed"
-FIELD_NAME_WIND_CHILL = "wind_chill"
-FIELD_NAME_WEATHER_CODE = "weather_code"
-FIELD_NAME_UV_LEVEL = "u_v_level"
-FIELD_NAME_UV_INDEX = "u_v_index"
-FIELD_NAME_UV_INDEX_MAX = "u_v_i_max"
-FIELD_NAME_LOCATION = "location"
-FIELD_NAME_UV_INDEX_FACTOR = "u_v_i_factor"
 
 LANGUAGES = ["en", "he"]
 
@@ -145,11 +146,12 @@ WIND_DIRECTIONS = {
 WEATHER_CODE_TO_CONDITION = {
     None: None,
     "0": None,
+    "1010": ATTR_CONDITION_EXCEPTIONAL,
     "1020": ATTR_CONDITION_LIGHTNING_RAINY,
     "1060": ATTR_CONDITION_SNOWY,
     "1070": ATTR_CONDITION_SNOWY,
     "1080": ATTR_CONDITION_SNOWY_RAINY,
-    "1140": ATTR_CONDITION_RAINY,
+    "1140": ATTR_CONDITION_POURING,
     "1160": ATTR_CONDITION_FOG,
     "1220": ATTR_CONDITION_PARTLYCLOUDY,
     "1230": ATTR_CONDITION_CLOUDY,
@@ -161,8 +163,8 @@ WEATHER_CODE_TO_CONDITION = {
     "1320": ATTR_CONDITION_HAIL,
     "1510": ATTR_CONDITION_LIGHTNING_RAINY,
     "1520": ATTR_CONDITION_SNOWY,
-    "1530": ATTR_CONDITION_PARTLYCLOUDY,
-    "1540": ATTR_CONDITION_PARTLYCLOUDY,
+    "1530": ATTR_CONDITION_RAINY,
+    "1540": ATTR_CONDITION_RAINY,
     "1560": ATTR_CONDITION_RAINY,
     "1570": ATTR_CONDITION_WINDY_VARIANT,
     "1580": ATTR_CONDITION_EXCEPTIONAL,
@@ -171,27 +173,27 @@ WEATHER_CODE_TO_CONDITION = {
 
 
 WEATHER_CODE_TO_ICON = {
-    "1250": "mdi:weather-sunny",
+    "1010": "mdi:weather-dust",
+    "1020": "mdi:weather-lightning-rainy",
+    "1060": "mdi:weather-snowy",
+    "1070": "mdi:weather-snowy",
+    "1080": "mdi:weather-snowy-rainy",
+    "1140": "mdi:weather-pouring",
+    "1160": "mdi:weather-fog",
     "1220": "mdi:weather-partly-cloudy",
     "1230": "mdi:weather-cloudy",
-    "1570": "mdi:weather-dust",
-    "1010": "mdi:weather-dust",
-    "1160": "mdi:weather-fog",
-    "1310": "mdi:weather-sunny-alert",
-    "1580": "mdi:weather-sunny-alert",
-    "1270": "mdi:weather-fog",
-    "1320": "mdi:snowflake-alert",
-    "1590": "mdi:snowflake-alert",
-    "1300": "mdi:snowflake-melt",
-    "1530": "mdi:weather-partly-rainy",
-    "1540": "mdi:weather-partly-rainy",
-    "1560": "mdi:weather-partly-rainy",
-    "1140": "mdi:weather-pouring",
-    "1020": "mdi:weather-lightning-rainy",
-    "1510": "mdi:weather-lightning",
+    "1250": "mdi:weather-sunny",
     "1260": "mdi:weather-windy",
-    "1080": "mdi:weather-snowy-rainy",
-    "1070": "mdi:weather-snowy-rainy",
-    "1060": "mdi:weather-snowy",
+    "1270": "mdi:weather-fog",
+    "1300": "mdi:snowflake-melt",
+    "1310": "mdi:weather-sunny-alert",
+    "1320": "mdi:snowflake-alert",
+    "1510": "mdi:weather-lightning",
     "1520": "mdi:weather-snowy-heavy",
+    "1530": "mdi:weather-partly-rainy",
+    "1540": "mdi:weather-rainy",
+    "1560": "mdi:weather-rainy",
+    "1570": "mdi:weather-dust",
+    "1580": "mdi:weather-sunny-alert",
+    "1590": "mdi:snowflake-alert",
 }
